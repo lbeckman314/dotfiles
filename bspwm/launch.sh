085c48a5c7a339b6c58c2155d4b0e5c79f169c12
lbeckman314 <lbeckman314@gmail.com>
2018-11-20 22:56:16 -0800

    updated bspwm config

--- bspwm/launch.sh
+++ bspwm/launch.sh
@@ -2,16 +2,7 @@
 
 # https://www.reddit.com/r/bspwm/comments/2mpui3/attempting_to_autostart_a_few_programs/
 # wait for an internet connection
-
-echo "waiting"
-while ! wget http://google.com -O- 2>/dev/null | grep -q Lucky; do
-   sleep .1
- done
-
-echo "launching!"
 # launch stuff
-xfce4-terminal &
-firefox &
-emacs &
-evince &
-vlc &
+xfce4-terminal
+#firefox &
+emacs
